d0ded41d08e69a9fb0380dadf22a092eef0268c1
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-11-29 10:19:26 +0530

    twoSum

---
+++ Array/twoSum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+    res_map = {}
+    for i in nums:
+        if target - i in res_map:
+            return [target-i,i]
+        else:
+            res_map[i] = i
+    return "No values."
+
+
+nums = [6,5,4,8,9,0]
+target = 10
+print(twoSum(nums, target))
