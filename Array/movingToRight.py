244cb0ae8d162f6504e425a2665c95b7c45d28e5
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-11-30 10:07:47 +0530

    new solution for moving target to right

--- Array/movingToRight.py
+++ Array/movingToRight.py
@@ -10,3 +10,16 @@ while i < j:
             nums[i], nums[j] = nums[j] , target
     i += 1
 print(nums)
+
+
+nums1 = [6,1,6,5,7,2,6,8,9,6]                            # O(n)T,   O(1)S
+print(nums1)
+i = 0
+count = 0
+while i < len(nums1):                                   #[1, 5, 7, 2, 8, 9, 6, 6, 6, 6]
+    if nums1[i] != target:
+        nums1[count] = nums1[i]
+        count += 1
+        nums1[i] = target
+    i += 1
+print(nums1)
